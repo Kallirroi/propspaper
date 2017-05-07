78169d4d2d50251982ef7f42a901316b4bdff586
Kallirroi <kallirroi.retzepi@gmail.com>
2017-05-07 16:43:41 -0400

    title is on top, two links at the bottom

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,19 +1,17 @@
 
 <!-- Header (logo/Browse button/Info/Search Input) -->
 <header class="header">
-
+	
+	<a class="title" href="<?php echo url() ?>">PROPS</a>
+	<div id="searchTerms"></div>
+	
 	<nav class="nav-alt">
-		<a href="<?php echo url() ?>">PROPS</a>
 		<a href="#" id="trigger-info-modal"> INFO </a>
 		<a href="#" id="indexButton">INDEX</a>
-		<a class="imageView" id="imageView"> IMAGE VIEW </a>
-		<a class="tagView" id="tagView"> TAG VIEW </a>	
-		<form class="nav-alt-search"> 
-			<input class="search-bar" type="text" value="" placeholder="Search..." data-active-placeholder="" name="s">
-		</form>		
+		<!-- <a class="imageView" id="imageView"> IMAGE VIEW </a> -->
+		<!-- <a class="tagView" id="tagView"> TAG VIEW </a>	 -->
 	</nav>
 
-	<div id="searchTerms"></div>
 
 
 </header>
