59310da5c4b81b850205883d104ab66fa5b822cb
Kallirroi <kallirroi.retzepi@gmail.com>
2017-05-16 14:22:08 -0400

    fixed problem with issue 9, I had to remove a .txt file from a photo that was deleted

--- site/accounts/kallirroi.php
+++ site/accounts/kallirroi.php
@@ -7,8 +7,8 @@ email: kallirroi.retzepi@gmail.com
 language: en
 role: admin
 history:
+  - issues/issue-9
   - issues/issue-9/issue-9
   - issues/issue-11/issue-11
   - issues/issue-2
   - issues/issue-2/1
-  - issues/issue-1/1
