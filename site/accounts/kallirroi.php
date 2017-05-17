37bae439f46f0d08ec3fadc0cfd3d12fb21de4c2
Kallirroi <kallirroi.retzepi@gmail.com>
2017-05-17 13:11:39 -0400

    added empty issues up till n.16 for them to populate

--- site/accounts/kallirroi.php
+++ site/accounts/kallirroi.php
@@ -7,6 +7,8 @@ email: kallirroi.retzepi@gmail.com
 language: en
 role: admin
 history:
-  - issues/issue-11/issue-11
-  - about
-  - issues/issue-10/issue-10
+  - issues/issue-16/issue-12
+  - issues/issue-16
+  - issues/issue-15/issue-12
+  - issues/issue-15
+  - issues/issue-14
