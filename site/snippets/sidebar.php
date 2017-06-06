016e924ba659567803ce0c17063b4677369e2dd9
Kallirroi <kallirroi.retzepi@gmail.com>
2017-06-06 19:08:26 -0400

    fixed an error - front pages now show in Sidebar

--- site/snippets/sidebar.php
+++ site/snippets/sidebar.php
@@ -4,7 +4,7 @@
 		<div class="verticalHandle"></div>
     </div>
 	<?php foreach($articlesSidebar->sortBy('title', 'desc') as $article): ?>
-		<?php if($image = $article->images()->filterBy('name', '*=', 'props')->sortBy('sort', 'asc')->first()): ?>
+		<?php if($image = $article->images()->filterBy('name', '*=', 'front')->sortBy('sort', 'asc')->first()): ?>
 			<a class="sidebarImages" href="<?php echo $article->url() ?>">
 				<img src="<?php echo thumb($image, array('width' => 515))->url() ?>" 
 				alt="<?php echo html($article->title()) ?>" />
