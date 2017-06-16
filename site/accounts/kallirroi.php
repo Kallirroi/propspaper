877a277960364fbeb2081f39e17c042682c43829
Kallirroi <kallirroi.retzepi@gmail.com>
2017-06-15 22:50:44 -0400

    having an image or GIF that is not exactly the same size as the rest will not change the layout

--- site/accounts/kallirroi.php
+++ site/accounts/kallirroi.php
@@ -7,8 +7,8 @@ email: kallirroi.retzepi@gmail.com
 language: en
 role: admin
 history:
+  - issues/issue-13/issue-13
   - issues/issue-1/1
   - issues/issue-3/issue-3
   - issues/issue-7/issue-7
   - issues/issue-10/issue-10
-  - contributors
